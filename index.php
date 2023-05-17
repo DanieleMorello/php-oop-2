dde7081cb9a74b180c0664765f1905e8ca54b778
DanieleMorello <danielemorello@live.it>
2023-05-17 17:34:12 +0200

    Refactoring index.php

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
 require_once __DIR__ . '/Model/Computer.php';
-
 require_once __DIR__ . '/Model/Desktop.php';
-
 require_once __DIR__ . '/Model/Laptop.php';
-
-require_once __DIR__ . '/Data/db.php'
-?>
-
-<?php
+require_once __DIR__ . '/Data/db.php';
 include __DIR__ . '/Views/Layout/head.php';
 include __DIR__ . '/Views/Layout/body.php';
-?>
